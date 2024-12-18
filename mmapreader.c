7e3f501174920f16b2493c2af1387a209f88f5c5
Kevin Lopez <kevinlo@ethz.ch>
2024-12-18 18:35:28 +0100

    [WIP] Reader and Writer Prototypes & Dirty Files

--- mmapreader.c
+++ mmapreader.c
@@ -16,10 +16,223 @@
 #include "cbor.h"
 #include "checksum.h"
 
-#define NPRMS 1
+#define NPRMS 2
 #define sharedFileIdIdx 0
 
 #define MDL_START
+
+
+static void indent(int nestingLevel)
+{
+    while (nestingLevel--)
+        printf("  ");
+}
+
+static void dumpbytes(const uint8_t *buf, size_t len)
+{
+    printf("\"");
+    while (len--)
+        printf("\\x%02X", *buf++);
+    printf("\"");
+}
+
+
+static CborError dumprecursive(CborValue *it, int nestingLevel)
+{
+    while (!cbor_value_at_end(it)) {
+        CborError err;
+        CborType type = cbor_value_get_type(it);
+
+        indent(nestingLevel);
+        switch (type) {
+        case CborArrayType:
+        case CborMapType: {
+            // recursive type
+            CborValue recursed;
+            assert(cbor_value_is_container(it));
+            printf(type == CborArrayType ? "Array[" : "Map[");
+            err = cbor_value_enter_container(it, &recursed);
+            if (err)
+                return err;       // parse error
+            err = dumprecursive(&recursed, nestingLevel + 1);
+            if (err)
+                return err;       // parse error
+            err = cbor_value_leave_container(it, &recursed);
+            if (err)
+                return err;       // parse error
+            indent(nestingLevel);
+            printf("]");
+            continue;
+        }
+
+        case CborIntegerType: {
+            int64_t val;
+            cbor_value_get_int64(it, &val);     // can't fail
+            printf("%lld\n", (long long)val);
+            break;
+        }
+
+        case CborByteStringType: {
+            uint8_t *buf;
+            size_t n;
+            err = cbor_value_dup_byte_string(it, &buf, &n, it);
+            if (err)
+                return err;     // parse error
+            dumpbytes(buf, n);
+            free(buf);
+            continue;
+        }
+
+        case CborTextStringType: {
+            char *buf;
+            size_t n;
+            err = cbor_value_dup_text_string(it, &buf, &n, it);
+            if (err)
+                return err;     // parse error
+            printf("\"%s\"\n", buf);
+            free(buf);
+            continue;
+        }
+
+        case CborTagType: {
+            CborTag tag;
+            cbor_value_get_tag(it, &tag);       // can't fail
+            printf("Tag(%lld)\n", (long long)tag);
+            break;
+        }
+
+        case CborSimpleType: {
+            uint8_t type;
+            cbor_value_get_simple_type(it, &type);  // can't fail
+            printf("simple(%u)\n", type);
+            break;
+        }
+
+        case CborNullType:
+            printf("null");
+            break;
+
+        case CborUndefinedType:
+            printf("undefined");
+            break;
+
+        case CborBooleanType: {
+            bool val;
+            cbor_value_get_boolean(it, &val);       // can't fail
+            printf(val ? "true" : "false");
+            break;
+        }
+
+        case CborDoubleType: {
+            double val;
+            if (false) {
+                float f;
+        case CborFloatType:
+                cbor_value_get_float(it, &f);
+                val = f;
+            } else {
+                cbor_value_get_double(it, &val);
+            }
+            printf("%g\n", val);
+            break;
+        }
+        case CborHalfFloatType: {
+            uint16_t val;
+            cbor_value_get_half_float(it, &val);
+            printf("__f16(%04x)\n", val);
+            break;
+        }
+
+        case CborInvalidType:
+            assert(false);      // can't happen
+            break;
+        }
+
+        err = cbor_value_advance_fixed(it);
+        if (err)
+            return err;
+    }
+    return CborNoError;
+}
+
+
+static
+void decode_cbor(
+    SimStruct* S,
+    const void* output_port,
+    DTypeId dtype,
+    int num_elems,
+    CborValue *it
+)
+{
+    /* Entering this subroutine implies you are a bus */
+    assert(cbor_value_is_container(it));
+
+    int i;
+    for (i = 0; i < num_elems; i++) {
+        DTypeId elem_type = ssGetBusElementDataType(S, dtype, i);
+        const char* elem_name = ssGetBusElementName(S, dtype, i);
+
+        if (ssIsDataTypeABus(S, elem_type) == 1) {
+            // cbor_value_map_find_value()
+        } else {
+            int_T offset = ssGetBusElementOffset(S, dtype, i);
+            CborValue value;
+
+            if (cbor_value_map_find_value(it, elem_name, &value) == CborInvalidType) {
+                printf("Could not find the element");
+            }
+            switch (elem_type) {
+                case SS_DOUBLE:
+                    if (cbor_value_is_container(&value)) {
+                        size_t num_rows;
+                        cbor_value_get_array_length(&value, &num_rows);
+                        CborValue row_it;
+                        size_t num_cols;
+
+                        cbor_value_enter_container(&value, &row_it);
+                        assert(cbor_value_is_container(&row_it));
+
+                        cbor_value_get_array_length(&row_it, &num_cols);
+
+                        size_t nbytes = sizeof(double) * (num_cols * num_rows);
+                        double* arr = (double*) malloc(nbytes);
+
+                        double *must_work = (double*) ((const char*) output_port + offset);
+
+                        int r;
+                        for (r = 0; r < num_rows; r++) {
+                            int c;
+                            CborValue col_it;
+                            for (c = 0; c < num_cols; c++) {
+                                cbor_value_enter_container(&row_it, &col_it);
+                                assert(cbor_value_is_container(&col_it));
+
+                                /* Matlab requires column major format */
+                                double val;
+                                cbor_value_get_double(&col_it, &val);
+                                arr[(c * num_cols) + r] = val;
+                                must_work[(c * num_cols) + r] = 10 * val;
+
+                                cbor_value_advance_fixed(&col_it);
+                            }
+
+                            cbor_value_advance(&row_it);
+                        }
+                        cbor_value_leave_container(&value, &row_it);
+
+                        for (r = 0; r < (num_cols * num_rows); r++){
+                            printf("%f ", must_work[r]);
+                        }
+                        printf("\n");
+                }
+            }
+
+        }
+    }
+}
+
+
 
 float
 extract_float(const uint8_t *buffer, size_t len)
@@ -89,15 +302,37 @@ mdlInitializeSizes(SimStruct *S)
 
     /* Allocate space for the pointer to shared memory. */
     ssSetNumPWork(S, 3);
+
+
+
+    DTypeId id;
+    ssRegisterTypeFromParameter(S, 1, &id);
+
+    if (id == INVALID_DTYPE_ID) {
+        report_and_exit("Registration Did not Work", S);
+    } else {
+        char *dtypeName = ssGetDataTypeName(S, id);
+        if(dtypeName == NULL) return;
+
+        ssSetOutputPortDataType(S, 0, id);
+        ssSetBusOutputObjectName(S, 0, dtypeName);
+
+        ssSetOutputPortWidth(S, 0, DYNAMICALLY_SIZED);
+        ssSetOutputPortComplexSignal(S, 0, COMPLEX_NO);
+        // ssSetOutputPortDirectFeedThrough(S, 0, 1);
+        // ssSetOutputPortRequiredContiguous(S, 0, 1);
+        ssSetBusOutputAsStruct(S, 0, true);
+        ssSetOutputPortBusMode(S, 0, SL_BUS_MODE);
+    }
+
 }
 
 static void
 mdlInitializeSampleTimes(SimStruct *S)
 {
-    ssSetSampleTime(S, 0, 0.01);
+    ssSetSampleTime(S, 0, 0.1);
     ssSetOffsetTime(S, 0, 0.0);
 }
-
 
 static void
 mdlOutputs(SimStruct *S, int_T tid)
@@ -105,19 +340,17 @@ mdlOutputs(SimStruct *S, int_T tid)
     RingBuffer* ring_buffer;
     ReadToken* read_token;
     Message msg;
-    real_T *y;
-    int_T width;
-    int_T i;
     real_T result;
     CborValue it;
     CborParser parser;
-    CborValue array_it;
-
-    y = ssGetOutputPortRealSignal(S,0);
-    width = ssGetOutputPortWidth(S,0);
 
     ring_buffer = (RingBuffer*)ssGetPWorkValue(S, 0);
     read_token = (ReadToken*)ssGetPWorkValue(S, 1);
+
+    void* output_port = ssGetOutputPortSignal(S, 0);
+    DTypeId dtype = ssGetOutputPortDataType(S, 0);
+    int num_elems = ssGetNumBusElements(S, dtype);
+
 
     msg = read_next(read_token, ring_buffer);
 
@@ -125,17 +358,15 @@ mdlOutputs(SimStruct *S, int_T tid)
         if ( crc((unsigned char *)&msg.data, CBOR_BUFFER_SIZE) != msg.checksum ) {
             return;
         }
-
-        cbor_parser_init(msg.data, 60, 0, &parser, &it);
-        cbor_value_enter_container(&it, &array_it);
-
-        for (i=0; i<width; i++) {
-            cbor_value_get_double(&array_it, &result);
-
-            *y++ = result;
-            cbor_value_advance_fixed(&array_it);
-        }
+        cbor_parser_init(msg.data, CBOR_BUFFER_SIZE, 0, &parser, &it);
+        // dumprecursive(&it, 0);
+        decode_cbor(S, output_port, dtype, num_elems, &it);
+        // printf("\n");
+    } else {
+        printf("Waiting\n");
     }
+
+    // printf("WTF %f\n", ((double*)output_port)[1]);
 
 }
 
--- mmapwriter.c
+++ mmapwriter.c
@@ -24,6 +24,37 @@
 #define MDL_START
 #define MDL_CHECK_PARAMETERS
 
+static
+void encode_array(
+    SimStruct* S,
+    CborEncoder* mapEncoder,
+    DTypeId nccarray,
+    const char* name,
+    const void* inputPort
+)
+{
+    cbor_encode_text_stringz(mapEncoder, name);
+
+    int_T offset_length = ssGetBusElementOffset(S, nccarray, 1);
+    printf("%Offset length %d\n", offset_length);
+    const uint8_t *length = (const uint8_t*) ((const char*) inputPort + offset_length);
+
+    // Its an 1D-Array
+    CborEncoder arrayEncoder;
+    cbor_encoder_create_array(mapEncoder, &arrayEncoder, *length);
+
+    int i;
+    int_T offset_array = ssGetBusElementOffset(S, nccarray, 0);
+    printf("%Offset array %d\n", offset_array);
+    const double *array = (const double*) ((const char*) inputPort + offset_array);
+
+    for (i = 0; i < *length; i++) {
+        cbor_encode_double(&arrayEncoder, *(array + i));
+    }
+
+    cbor_encoder_close_container(mapEncoder, &arrayEncoder);
+
+}
 
 static
 void encode_cbor(
@@ -32,7 +63,10 @@ void encode_cbor(
     const void* inputPort,
     DTypeId elemType,
     int numInnerElems,
-    int level
+    int level,
+    int* counter,
+    int32_t* inputSizes,
+    int_T nrows
 )
 {
     CborEncoder mapEncoder;
@@ -41,25 +75,98 @@ void encode_cbor(
     int i;
     for (i = 0; i < numInnerElems; i++) {
         DTypeId subDtype = ssGetBusElementDataType(S, elemType, i);
+        // printf("%s\n", ssGetBusElementName(S, elemType, i));
 
         if (ssIsDataTypeABus(S, subDtype) == 1) {
+            // printf("%s| %s \n", ssGetBusElementName(S, elemType, i), ssGetDataTypeName(S, subDtype));
+            // if (strcmp(ssGetDataTypeName(S, subDtype), "NCCArray") == 0) {
+            //     int_T offset = ssGetBusElementOffset(S, elemType, i);
+            //     encode_array(S, &mapEncoder, subDtype, ssGetBusElementName(S, elemType, i), inputPort + offset);
+            // } else {
             cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
 
             int_T offset = ssGetBusElementOffset(S, elemType, i);
-            encode_cbor(S, &mapEncoder, inputPort + offset, subDtype, ssGetNumBusElements(S, subDtype), (level + 1));
+            encode_cbor(S,
+                        &mapEncoder,
+                        inputPort + offset,
+                        subDtype,
+                        ssGetNumBusElements(S, subDtype),
+                        (level + 1),
+                        counter,
+                        inputSizes,
+                        nrows);
+            // }
         }
         else {
+            int_T offset = ssGetBusElementOffset(S, elemType, i);
+
+            // Array is 2D Dimensional Array with column major order.
+            int element_dims[2];
+            element_dims[0] = inputSizes[*counter];
+            element_dims[1] = inputSizes[(1 * nrows) + (*counter)];
+
+            // printf("Index: %d\n", *counter);
+            *counter = *counter + 1;
+
+            // serialize_array(&mapEncoder, subDtype, name, address, element_dims);
+
+
             switch (subDtype) {
                 case SS_DOUBLE:
-                    int_T offset = ssGetBusElementOffset(S, elemType, i);
                     /* Assume that the bus element is of type double */
-                    const double *in = (const double*) ((const char*)inputPort + offset);
+                    const double *double_in = (const double*) ((const char*)inputPort + offset);
 
                     cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
-                    cbor_encode_double(&mapEncoder, *in);
 
-                    // printf("Level: %d | Elements %d | SS_DOUBLE %f\n", level,  numInnerElems, *in);
-                    // printf("%s\n", ssGetBusElementName(S, elemType, i));
+                    CborEncoder arrayEncoder;
+                    cbor_encoder_create_array(&mapEncoder, &arrayEncoder, element_dims[0]);
+
+                    int r, c;
+                    for (r = 0; r < element_dims[0]; r++) {
+                        CborEncoder rowEncoder;
+                        cbor_encoder_create_array(&arrayEncoder, &rowEncoder, element_dims[1]);
+                        for (c = 0; c < element_dims[1]; c++) {
+                            cbor_encode_double(&rowEncoder, double_in[(element_dims[0] * c) + r]);
+                        }
+                        cbor_encoder_close_container(&arrayEncoder, &rowEncoder);
+                    }
+
+                    cbor_encoder_close_container(&mapEncoder, &arrayEncoder);
+                    break;
+
+                case SS_SINGLE:
+                    const float *float_in = (const float*) ((const char*)inputPort + offset);
+
+                    cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
+                    cbor_encode_float(&mapEncoder, *float_in);
+                    break;
+                case SS_INT8:
+                case SS_INT16:
+                case SS_INT32:
+                    /* CBOR Doest not provide a function to encode int8 or int16, so we cast it as
+                    a normal integer */
+                    const long int *int_in = (const long int*) ((const char*)inputPort + offset);
+
+                    cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
+                    cbor_encode_int(&mapEncoder, *int_in);
+                    break;
+                case SS_UINT8:
+                case SS_UINT16:
+                case SS_UINT32:
+                    /* CBOR Doest not provide a function to encode uint8 or uint16, so we cast it as
+                    a normal unsigned integer */
+                    const unsigned long int *uint_in = (const unsigned long int*) ((const char*)inputPort + offset);
+
+                    cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
+                    cbor_encode_uint(&mapEncoder, *uint_in);
+                    break;
+                default:
+                    const int *enum_value_in = (const int*) ((const char*) inputPort + offset);
+                    if (dtaGetDataTypeIsEnumType(ssGetDataTypeAccess(S), ssGetPath(S), subDtype) == 1){
+                        // printf("%s\n", dtaGetEnumTypeStringFromValue(ssGetDataTypeAccess(S), ssGetPath(S), subDtype, *enum_value_in));
+                        cbor_encode_text_stringz(&mapEncoder, ssGetBusElementName(S, elemType, i));
+                        cbor_encode_int(&mapEncoder, *enum_value_in);
+                    }
                     break;
             }
         }
@@ -145,7 +252,7 @@ mdlInitializeSizes(SimStruct *S)
         return; /* Parameter mismatch reported by the Simulink engine*/
     }
 
-    if (!ssSetNumInputPorts(S, 1)) return;
+    if (!ssSetNumInputPorts(S, 2)) return;
 
     DTypeId id;
     // ssRegisterTypeFromNamedObject(S, "TopBus", &id);
@@ -162,6 +269,17 @@ mdlInitializeSizes(SimStruct *S)
         ssSetBusInputAsStruct(S, 0, 1);
         ssSetInputPortBusMode(S, 0, SL_BUS_MODE);
     }
+
+    ssSetInputPortDirectFeedThrough(S, 1, 1);
+        /* Input is a real signal */
+    ssSetInputPortComplexSignal(S, 1, COMPLEX_NO);
+    /* Input is a dynamically sized 2-D matrix */
+    ssSetInputPortMatrixDimensions(S ,1, DYNAMICALLY_SIZED, DYNAMICALLY_SIZED);
+    /* Input inherits its sample time */
+    ssSetInputPortSampleTime(S, 1, INHERITED_SAMPLE_TIME);
+    /* Input signal must be contiguous */
+    ssSetInputPortRequiredContiguous(S, 1, 1);
+    ssSetInputPortDataType(S, 1, SS_INT32);
 
 
     mdlCheckParameters(S);
@@ -198,27 +316,18 @@ mdlOutputs(SimStruct *S, int_T tid)
     const void* inputPort = ssGetInputPortSignal(S, 0);
     DTypeId dType = ssGetInputPortDataType(S, 0);
     int numElems = ssGetNumBusElements(S, dType);
-    // printf("Bus with n: %d\n", numElems);
-    //
-    // int i;
-    // for (i = 0; i < numElems; i++) {
-    //     int_T elemType = ssGetBusElementDataType(S, dType, i);
-    //
-    //     if (ssIsDataTypeABus(S, elemType) == 1) {
-    //         /* Sub-bus element */
-    //         printf("Element %d is a sub-bus\n", i);
-    //         printf("Sub bus with n: %d\n", ssGetNumBusElements(S, elemType));
-    //     } else if (elemType == SS_DOUBLE) {
-    //         printf("Element %d is a double\n", i);
-    //     }
-    // }
 
-    // publish(ring_buffer, uPtrs, inp_width);
     CborEncoder encoder;
     unsigned int cbor_buffer_size = ring_buffer->cbor_buffer_size;
 
+    const void* inputPortSizes = ssGetInputPortSignal(S, 1);
+    int_T* dims_flat_bus  = ssGetInputPortDimensions(S, 1);
+    int counter = 0;
+
     cbor_encoder_init(&encoder, cborBuffer, cbor_buffer_size, 0);
-    encode_cbor(S, &encoder, inputPort, dType, numElems, 0);
+
+    encode_cbor(S, &encoder, inputPort, dType, numElems, 0, &counter, (int32_t*) inputPortSizes, dims_flat_bus[0]);
+
     publish(ring_buffer, cborBuffer, cbor_buffer_size);
 }
 
